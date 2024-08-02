93a15a9a4b1c1c4d3b94adc1c6403ee95f865bd0
Seaoak <seaoak@seaoak.jp>
2024-08-02 20:46:37 +0900

    Implement POP3 bridge

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,160 @@
 use std::env;
-use std::net::TcpStream;
+use std::net::{IpAddr, Ipv4Addr, TcpListener, TcpStream};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use lazy_static::lazy_static;
 use native_tls::TlsConnector;
+use regex::Regex;
 
 mod my_disconnect;
 mod my_text_line_stream;
 mod pop3_upstream;
 
+use my_text_line_stream::MyTextLineStream;
 use pop3_upstream::*;
 
 //====================================================================
 fn main() {
     println!("Hello, world!");
 
-    match test_pop3() {
+    match test_pop3_bridge() {
         Ok(()) => (),
-        Err(e) => panic!("{}", e),
+        Err(e) => panic!("{:?}", e),
+    };
+
+    match test_pop3_upstream() {
+        Ok(()) => (),
+        Err(e) => panic!("{:?}", e),
     };
 }
 
-fn test_pop3() -> Result<()> {
+lazy_static!{
+    static ref REGEX_POP3_COMMAND_LINE_FOR_MULTI_LINE_RESPONSE: Regex = Regex::new(r"^(LIST|UIDL|RETR +\S+|TOP +\S+( +\S+)?)\s*\r\n$").unwrap();
+    static ref REGEX_POP3_COMMAND_LINE_FOR_QUIT: Regex = Regex::new(r"^QUIT\s*\r\n$").unwrap();
+}
+
+#[allow(unused)]
+fn test_pop3_bridge() -> Result<()> {
+    let upstream_hostname = env::var("FUBACO_Km2TTTAEMErD_H").unwrap();
+    let upstream_port = 995;
+
+    // https://doc.rust-lang.org/std/net/struct.TcpListener.html
+    let downstream_port = 5940;
+    let downstream_addr = format!("{}:{}", "127.0.0.1", downstream_port);
+    let listener = TcpListener::bind(downstream_addr)?;
+    loop {
+        match listener.accept() {
+            Ok((downstream_tcp_stream, remote_addr)) => {
+                // https://doc.rust-lang.org/std/net/enum.SocketAddr.html#method.ip
+                assert_eq!(remote_addr.ip(), IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+
+                let mut downstream_stream = MyTextLineStream::connect(downstream_tcp_stream);
+
+                println!("open upstream connection");
+                let upstream_connector = TlsConnector::new()?;
+                let upstream_tcp_stream = TcpStream::connect((upstream_hostname.to_string(), upstream_port))?;
+                let upstream_tls_stream = upstream_connector.connect(&upstream_hostname, upstream_tcp_stream)?;
+                let mut upstream_stream = MyTextLineStream::connect(upstream_tls_stream);
+
+                // wait for POP3 greeting message from server
+                {
+                    let mut response = Vec::<u8>::new();
+                    if let Err(e) = upstream_stream.read_some_lines(&mut response) {
+                        if let Err(e2) = downstream_stream.disconnect() {
+                            return Err(anyhow!("DOUBLE FAILURE!\nError1: {:?}\nError2: {:?}", e, e2));
+                        }
+                        return Err(e);
+                    }
+                    println!("relay greeting message");
+                    if let Err(e) = downstream_stream.write_all_and_flush(&response) {
+                        if let Err(e2) = upstream_stream.disconnect() {
+                            return Err(anyhow!("DOUBLE FAILURE!\nError1: {:?}\nError2: {:?}", e, e2));
+                        }
+                        return Err(e);
+                    }
+                    println!("Done");
+                }
+
+                // relay POP3 commands/responses
+                loop {
+                    let is_multi_line_response_expected;
+                    let is_last_command;
+                    { // relay a POP3 command
+                        let mut command_line = Vec::<u8>::new();
+                        if let Err(e) = downstream_stream.read_some_lines(&mut command_line) {
+                            if let Err(e2) = upstream_stream.disconnect() {
+                                return Err(anyhow!("DOUBLE FAILURE!\nError1: {:?}\nError2: {:?}", e, e2));
+                            }
+                            return Err(e);
+                        }
+                        let command_str = String::from_utf8_lossy(&command_line);
+                        println!("relay POP3 command: {}", command_str);
+                        if let Err(e) = upstream_stream.write_all_and_flush(&command_line) {
+                            if let Err(e2) = downstream_stream.disconnect() {
+                                return Err(anyhow!("DOUBLE FAILURE!\nError1: {:?}\nError2: {:?}", e, e2));
+                            }
+                            return Err(e);
+                        }
+                        println!("Done");
+                        is_multi_line_response_expected = REGEX_POP3_COMMAND_LINE_FOR_MULTI_LINE_RESPONSE.is_match(&command_str);
+                        is_last_command = REGEX_POP3_COMMAND_LINE_FOR_QUIT.is_match(&command_str);
+                    }
+
+                    let mut response_lines = Vec::<u8>::new();
+                    let mut is_first_response = true;
+                    let mut status_line = "".to_string(); // dummy initialization (must be set to a string before use)
+                    loop { // receive lines until the end of a response
+                        if let Err(e) = upstream_stream.read_some_lines(&mut response_lines) {
+                            if let Err(e2) = downstream_stream.disconnect() {
+                                return Err(anyhow!("DOUBLE FAILURE!\nError1: {:?}\nError2: {:?}", e, e2));
+                            }
+                            return Err(e);
+                        }
+                        if (is_first_response) {
+                            status_line = MyTextLineStream::<TcpStream>::take_first_line(&response_lines)?;
+                        }
+                        if !is_multi_line_response_expected {
+                            println!("relay single-line response: {}", status_line);
+                            break;
+                        }
+                        if is_first_response && status_line.starts_with("-ERR") {
+                            println!("relay ERR response: {}", status_line);
+                            break;
+                        }
+                        if MyTextLineStream::<TcpStream>::ends_with_u8(&response_lines, b"\r\n.\r\n") {
+                            println!("relay multl-line response ({} byte body): {}", response_lines.len() - status_line.len() - b".\r\n".len(), status_line);
+                            break;
+                        }
+                        is_first_response = false;
+                    }
+                    if let Err(e) = downstream_stream.write_all_and_flush(&response_lines) {
+                        if let Err(e2) = upstream_stream.disconnect() {
+                            return Err(anyhow!("DOUBLE FAILURE!\nError1: {:?}\nError2: {:?}", e, e2));
+                        }
+                        return Err(e);
+                    }
+                    println!("Done");
+                    if is_last_command {
+                        println!("close POP3 stream");
+                        upstream_stream.disconnect()?;
+                        downstream_stream.disconnect()?;
+                        break;
+                    }
+                }
+            },
+            Err(e) => return Err(anyhow!(e)),
+        }
+    }
+}
+
+#[allow(unused)]
+fn test_pop3_upstream() -> Result<()> {
     let username = env::var("FUBACO_Nq2DYd4cFHGZ_U").unwrap();
     let password = env::var("FUBACO_AhCE3FNtfdJV_P").unwrap();
     let hostname = env::var("FUBACO_Km2TTTAEMErD_H").unwrap();
     let port = 995;
 
-    println!("open connection");
+    println!("open upstream connection");
     let connector = TlsConnector::new()?;
     let tcp_stream = TcpStream::connect((hostname.to_string(), port))?;
     let tls_stream = connector.connect(&hostname, tcp_stream)?;
--- src/my_text_line_stream.rs
+++ src/my_text_line_stream.rs
@@ -5,6 +5,8 @@ use anyhow::{anyhow, Result};
 use crate::my_disconnect::MyDisconnect;
 
 const ALMOST_MAX_LINE_LENGTH: usize = 1024;
+const ASCII_CODE_CR: u8 = b'\r'; // 0x0d "Carriage Return"
+const ASCII_CODE_LF: u8 = b'\n'; // 0x0a "Line Feed"
 
 //====================================================================
 #[derive(Debug)]
@@ -25,7 +27,7 @@ impl<S: Read + Write + MyDisconnect> MyTextLineStream<S> {
     }
 
     pub fn write_all_and_flush(&mut self, lines: &[u8]) -> Result<()> {
-        assert!(ends_with_u8(lines, b"\r\n"));
+        assert!(Self::ends_with_u8(lines, b"\r\n"));
         self.raw_stream.write_all(lines)?;
         self.raw_stream.flush()?;
         Ok(())
@@ -43,23 +45,52 @@ impl<S: Read + Write + MyDisconnect> MyTextLineStream<S> {
                 Err(e) => return Err(anyhow!(e)),
             };
             buf.extend(&local_buf[0..nbytes]);
-            if ends_with_u8(buf, b"\r\n") { // allow empty line
+            if Self::ends_with_u8(buf, b"\r\n") { // allow empty line
                 break;
             }
         }
         Ok(())
     }
-}
 
-//====================================================================
-fn ends_with_u8(target: &[u8], pattern: &[u8]) -> bool {
-    if target.len() < pattern.len() {
-        return false;
-    }
-    for i in 1..=pattern.len() {
-        if target[target.len() - i] != pattern[pattern.len() - i] {
+    // static methods (utilities)
+    #[allow(unused)]
+    pub fn starts_with_u8(target: &[u8], pattern: &[u8]) -> bool {
+        if target.len() < pattern.len() {
             return false;
         }
+        for i in 0..pattern.len() {
+            if target[i] != pattern[i] {
+                return false;
+            }
+        }
+        true
     }
-    true
+    
+    #[allow(unused)]
+    pub fn ends_with_u8(target: &[u8], pattern: &[u8]) -> bool {
+        if target.len() < pattern.len() {
+            return false;
+        }
+        for i in 1..=pattern.len() {
+            if target[target.len() - i] != pattern[pattern.len() - i] {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[allow(unused)]
+    pub fn take_first_line(target: &[u8]) -> Result<String> { // return value includes CRLF
+        let pos_of_first_crlf = target.iter().position(|c| *c == ASCII_CODE_CR);
+        if let Some(pos) = pos_of_first_crlf {
+            if target.len() < pos + 2 || target[pos + 1] != ASCII_CODE_LF {
+                return Err(anyhow!("detect CR character without following LF character: {}", String::from_utf8_lossy(target)));
+            }
+            let mut buf = Vec::<u8>::with_capacity(pos + 2);
+            buf.extend(target[0..pos + 2].iter());
+            let first_line = String::from_utf8_lossy(&buf).to_string();
+            return Ok(first_line);
+        }
+        Err(anyhow!("detect lack of CRLF: {}", String::from_utf8_lossy(target)))
+    }
 }
