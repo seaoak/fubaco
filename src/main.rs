5a69bfec673533a1f2196599ca7eb822de12f13d
Seaoak <seaoak@seaoak.jp>
2024-08-09 17:10:59 +0900

    Fix bugs when "Return-Path" field has empty value

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ lazy_static!{
 
 fn spam_checker_blacklist_tld(message: &Message) -> Option<String> {
     let blacklist_tld_list = vec![".cn", ".ru", ".hu", ".br", ".su", ".nz", ".in", ".cz", ".be"];
-    let header_from = message.from().unwrap().first().unwrap().address.clone().unwrap().to_string();
+    let header_from = message.from().unwrap().first().map(|addr| addr.address.clone().unwrap_or_default().to_string()).unwrap_or_default();
     let envelop_from = message.return_path().clone().unwrap_text().to_string().replace("<", "").replace(">", "");
     let target_addresses = [header_from, envelop_from];
     let is_spam = (|| {
