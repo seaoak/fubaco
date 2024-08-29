2db3ba3d0eab2366ac991cf1f70ce24e0e6c6924
Seaoak <seaoak@seaoak.jp>
2024-08-29 12:55:42 +0900

    Remove dependency to "win-crypto-ng" crate (use "Sha1" and "Sha2" crates instead)

--- src/my_crypto.rs
+++ src/my_crypto.rs
@@ -1,30 +1,25 @@
 use anyhow::{bail, Result};
 use rsa::{RsaPublicKey, pkcs1::DecodeRsaPublicKey, pkcs1v15::Pkcs1v15Sign, pkcs8::DecodePublicKey};
-use sha1::Sha1;
+use sha1::{Digest, Sha1};
 use sha2::Sha256;
-use win_crypto_ng::hash::{HashAlgorithm, HashAlgorithmId};
 
 #[allow(unused)]
 pub fn my_calc_sha1(input: &[u8]) -> Vec<u8> {
-    let algo = HashAlgorithm::open(HashAlgorithmId::Sha1).unwrap();
-    let mut hash = algo.new_hash().unwrap();
-    hash.hash(input).unwrap();
-    let result = hash.finish().unwrap();
-    assert_eq!(result.len(), 160 / 8);
-    let mut buf = Vec::<u8>::new();
-    buf.extend(result.as_slice().into_iter());
+    let mut hasher = Sha1::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    let buf = result.to_vec();
+    assert_eq!(buf.len(), 160 / 8);
     buf
 }
 
 #[allow(unused)]
 pub fn my_calc_sha256(input: &[u8]) -> Vec<u8> {
-    let algo = HashAlgorithm::open(HashAlgorithmId::Sha256).unwrap();
-    let mut hash = algo.new_hash().unwrap();
-    hash.hash(input).unwrap();
-    let result = hash.finish().unwrap();
-    assert_eq!(result.len(), 256 / 8);
-    let mut buf = Vec::<u8>::new();
-    buf.extend(result.as_slice().into_iter());
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    let buf = result.to_vec();
+    assert_eq!(buf.len(), 256 / 8);
     buf
 }
 
