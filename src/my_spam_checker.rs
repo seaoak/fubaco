56730735855b71204fbf065744e1c6f87c796908
Seaoak <seaoak@seaoak.jp>
2024-10-24 10:39:05 +0900

    Improve "normalize_string()" (remove non-alphanumeric Unicode characters)

--- src/my_spam_checker.rs
+++ src/my_spam_checker.rs
@@ -20,9 +20,18 @@ lazy_static! {
 fn normalize_string<P: AsRef<str>>(s: P) -> String {
     // normalize string (Unicode NFKC, uppercase, no-whitespace, no-bullet)
     let s: &str = s.as_ref();
-    let mut unicode_normalized_str = String::new();
-    unicode_normalized_str.extend(s.nfkc());
-    wide2ascii(&unicode_normalized_str).to_uppercase().replace(&[' ', '　', '・'], "")
+    let s = s.nfkc();
+    let s = String::from_iter(s);
+    let s = wide2ascii(&s);
+    let s = s.chars();
+    let s = s.filter(|c| !c.is_control());
+    let s = s.filter(|c| !c.is_whitespace());
+    // let s = s.filter(|c| !c.is_ascii_graphic());
+    let s = s.filter(|c| c.is_alphanumeric() || *c == '@' || *c == '.' || *c == '-' || *c == '_');
+    let s = String::from_iter(s);
+    // let s = s.replace(&[' ', '　', '・'], "");
+    let s = s.to_uppercase();
+    s
 }
 
 fn load_tsv_file<P: AsRef<Path>>(path: P) -> Result<Vec<Vec<String>>> {
