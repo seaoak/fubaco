8b107bb93b62faf1851ade00fa5a6bfa3a4f80ca
Seaoak <seaoak@seaoak.jp>
2025-08-08 09:44:41 +0900

    Implement `run.sh`

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+test -f fubaco.log && mv fubaco.log fubaco.log.bak
+test -f dns_cache.json && cp -p dns_cache.json dns_cache.json.bak
+
+test -f .env && source .env
+export RUST_BACKTRACE=1
+
+rustup -V
+rustup show -v
+
+cargo run
