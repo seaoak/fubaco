b21681ac6ed9af31dfad2af26ecc4ee1c927144a
Seaoak <seaoak@seaoak.jp>
2025-08-24 14:28:31 +0900

    Update `run.sh`

--- run.sh
+++ run.sh
@@ -7,9 +7,11 @@ test -f fubaco.log && mv fubaco.log fubaco.log.bak
 test -f dns_cache.json && cp -p dns_cache.json dns_cache.json.bak
 
 test -f .env && source .env
+
 export RUST_BACKTRACE=1
+#export RUST_BACKTRACE=full
 
 rustup -V
 rustup show -v
 
-cargo run
+exec cargo run
